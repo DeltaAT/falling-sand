32e571fd44fd0e2aa5c04ffd91ea991a88e7273e
DeltaAT <eliaspoe194@gmail.com>
2026-07-02 19:41:16 +0200

    initialized window and macroquad things

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+const GRID_H: u8 = 220;
+const GRID_W: u8 = 160;
+const SCALE: f32 = 4.0;
+
+fn win_conf() -> Conf {
+    Conf {
+        window_title: "Falling Sand".to_owned(),
+        window_width: (GRID_W as f32 * SCALE) as i32,
+        window_height: (GRID_H as f32 * SCALE) as i32,
+        window_resizable: false,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(win_conf)]
+async fn main() {
+    println!("Falling Sand!");
+    loop {
+        clear_background(BLACK);
+
+        draw_text("Hello from Delta", 20.0, 20.0, 30.0, WHITE);
+
+        next_frame().await
+    }
 }
